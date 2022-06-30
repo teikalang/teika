521963a97fe11ab2eb6032e345402791dd9045e4
EduardoRFS <theeduardorfs@gmail.com>
2022-06-30 13:59:51 +0100

    smol: define Type module
    This is gonna be used in the traditional way through substitutions.

---
+++ smol/type.mli
@@ -0,0 +1,16 @@
+type type_ = private
+  | T_var of { var : Var.t }
+  | T_arrow of { param : type_; return : type_ }
+  | T_forall of { var : Var.t; return : type_ }
+  | T_pair of { left : type_; right : type_ }
+  | T_exists of { var : Var.t; right : type_ }
+  | T_type of { type_ : type_ }
+
+type t = type_ [@@deriving show]
+
+val t_var : var:Var.t -> type_
+val t_arrow : param:type_ -> return:type_ -> type_
+val t_forall : var:Var.t -> return:type_ -> type_
+val t_pair : left:type_ -> right:type_ -> type_
+val t_exists : var:Var.t -> right:type_ -> type_
+val t_type : type_:type_ -> type_
