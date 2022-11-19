d9acf25e7f6018b9a4c8f9f4a5809f7f5a616ce0
EduardoRFS <theeduardorfs@gmail.com>
2022-11-19 14:54:03 -0500

    teika: fix #64 missing predefined Type

--- teika/var.mli
+++ teika/var.mli
@@ -4,4 +4,8 @@ type t = var [@@deriving show, eq, ord]
 val create : Name.t -> var
 val name : var -> Name.t
 
+(* predefined *)
+(* Type *)
+val type_ : var
+
 module Map : Map.S with type key = var
