d45d3b1ca516c3eed1e5afd85ee53385598872dc
EduardoRFS <theeduardorfs@gmail.com>
2022-11-23 18:04:17 -0300

    teika: prepare context for substitutions

--- teika/context.ml
+++ teika/context.ml
@@ -1,225 +1,63 @@
 open Ttree
 
-type var_info =
-  | V_rigid of { level : Level.t }
-  | V_alias of { var : Var.t }
-  | V_link of { type_ : term_desc }
-  | V_hole of { level : Level.t }
-
 type error = CError of { loc : Location.t; desc : error_desc }
+and error_desc = |
 
-and error_desc =
-  | CError_occurs_check of { var : Var.t; in_ : Var.t }
-  | CError_escape_check of { var : Var.t; to_ : Var.t }
-  | CError_var_constrained of { var : Var.t; by_ : term_desc }
-  | CError_var_clash of { expected : Var.t; received : Var.t }
-  | CError_type_clash of { expected : term_desc; received : term_desc }
-  (* invariants *)
-  | CError_unknown_var_repr of { var : Var.t }
-  | CError_unknown_var_link of { var : Var.t }
-  | CError_unknown_var_lower of { var : Var.t }
-  | CError_unknown_var_alias of { var : Var.t }
-  | CError_duplicated_var_rigid of { var : Var.t }
-  | CError_duplicated_var_alias of { var : Var.t }
-  | CError_duplicated_var_hole of { var : Var.t }
-  | CError_invalid_var_link of { var : Var.t; info : var_info }
-  | CError_invalid_var_lower of { var : Var.t; info : var_info }
-  | CError_invalid_var_alias of { var : Var.t; info : var_info }
-  | CError_lowering_to_higher_level of { var : Var.t }
+module Subst_context = struct
+  type 'a subst_context = {
+    (* TODO: accumulate locations during substitutions *)
+    context :
+      'k.
+      from:Offset.t ->
+      to_:term_desc ->
+      ok:('a -> 'k) ->
+      error:(error_desc -> 'k) ->
+      'k;
+  }
+  [@@ocaml.unboxed]
 
-type data = {
-  loc : Location.t;
-  (* unification *)
-  level : Level.t;
-  (* TODO: for variables this could be done using an array
-      as even in big programs the number of variables is relatively
-      small, unless we start generating variables during typing *)
-  vars : var_info Var.Map.t;
-}
+  type 'a t = 'a subst_context
 
-(* TODO: I'm bad with monads *)
-type 'a context = Context of (data -> ('a * data, error * data) result)
-[@@ocaml.unboxed]
+  let[@inline always] test ~loc ~from ~to_ f =
+    let { context } = f () in
+    let ok value = Ok value in
+    let error desc = Error (CError { loc; desc }) in
+    context ~from ~to_ ~ok ~error
 
-type 'a t = 'a context
+  let[@inline always] return value =
+    let context ~from:_ ~to_:_ ~ok ~error:_ = ok value in
+    { context }
 
-let return content = Context (fun data -> Ok (content, data))
+  let[@inline always] bind context f =
+    let { context } = context in
+    let context ~from ~to_ ~ok ~error =
+      let ok data =
+        let { context } = f data in
+        context ~from ~to_ ~ok ~error
+      in
+      context ~from ~to_ ~ok ~error
+    in
+    { context }
 
-let fail desc =
-  Context
-    (fun data ->
-      let { loc; level = _; vars = _ } = data in
-      Error (CError { loc; desc }, data))
+  let ( let* ) = bind
 
-let apply context data f =
-  let (Context context) = context in
-  match context data with
-  | Ok (content, data) -> f content data
-  | Error _error as error -> error
+  let[@inline always] ( let+ ) context f =
+    let* value = context in
+    return @@ f value
 
-let ( >>= ) context f =
-  Context
-    (fun data ->
-      let (Context context) = context in
-      match context data with
-      | Ok (content, data) ->
-          let (Context f) = f content in
-          f data
-      | Error _error as error -> error)
+  let[@inline always] from () =
+    let context ~from ~to_:_ ~ok ~error:_ = ok from in
+    { context }
 
-let fail_occurs_check var ~in_ = fail (CError_occurs_check { var; in_ })
-let fail_escape_check var ~to_ = fail (CError_escape_check { var; to_ })
-let fail_var_constrained var ~by_ = fail (CError_var_constrained { var; by_ })
+  let[@inline always] to_ () =
+    let context ~from:_ ~to_ ~ok ~error:_ = ok to_ in
+    { context }
 
-let fail_var_clash ~expected ~received =
-  fail (CError_var_clash { expected; received })
-
-let fail_type_clash ~expected ~received =
-  fail (CError_type_clash { expected; received })
-
-let loc =
-  Context
-    (fun data ->
-      let { loc; level = _; vars = _ } = data in
-      Ok (loc, data))
-
-let level =
-  Context
-    (fun data ->
-      let { loc = _; level; vars = _ } = data in
-      Ok (level, data))
-
-let repr var =
-  Context
-    (fun data ->
-      let { loc; level = _; vars } = data in
-      match Var.Map.find_opt var vars with
-      | Some var_info -> Ok (var_info, data)
-      | None ->
-          let desc = CError_unknown_var_repr { var } in
-          Error (CError { loc; desc }, data))
-
-let with_loc loc f =
-  Context
-    (fun data ->
-      let { loc = initial_loc; level; vars } = data in
-      apply (f ()) { loc; level; vars } @@ fun content data ->
-      let { loc = _closing_loc; level; vars } = data in
-      Ok (content, { loc = initial_loc; level; vars }))
-
-let with_region f =
-  Context
-    (fun data ->
-      let { loc; level = initial_level; vars } = data in
-      let level = Level.next initial_level in
-      apply (f ()) { loc; level; vars } @@ fun content data ->
-      let { loc; level = _closing_level; vars } = data in
-      (* TODO: check no variable remaining on the level before closing *)
-      Ok (content, { loc; level = initial_level; vars }))
-
-(* TODO: duplicated code *)
-let with_var_rigid var f =
-  Context
-    (fun data ->
-      let { loc; level; vars } = data in
-      match Var.Map.mem var vars with
-      | true ->
-          let desc = CError_duplicated_var_rigid { var } in
-          Error (CError { loc; desc }, data)
-      | false ->
-          let info = V_rigid { level } in
-          let vars = Var.Map.add var info vars in
-          apply (f ()) { loc; level; vars } @@ fun content data ->
-          let { loc; level; vars } = data in
-          (* TODO: check variable did not escape it's scope *)
-          let vars = Var.Map.remove var vars in
-          Ok (content, { loc; level; vars }))
-
-(* TODO: duplicated code *)
-let with_var_alias var ~of_ f =
-  Context
-    (fun data ->
-      let { loc; level; vars } = data in
-      match Var.Map.mem var vars with
-      | true ->
-          let desc = CError_duplicated_var_alias { var } in
-          Error (CError { loc; desc }, data)
-      | false ->
-          let info = V_alias { var = of_ } in
-          let vars = Var.Map.add var info vars in
-          apply (f ()) { loc; level; vars } @@ fun content data ->
-          let { loc; level; vars } = data in
-          (* TODO: check variable did not escape it's scope *)
-          let vars = Var.Map.remove var vars in
-          Ok (content, { loc; level; vars }))
-
-let enter_var_hole var =
-  Context
-    (fun data ->
-      let { loc; level; vars } = data in
-      match Var.Map.mem var vars with
-      | true ->
-          let desc = CError_duplicated_var_hole { var } in
-          Error (CError { loc; desc }, data)
-      | false ->
-          let info = V_hole { level } in
-          let vars = Var.Map.add var info vars in
-          Ok ((), { loc; level; vars }))
-
-let var_link var ~to_ =
-  (* TODO: path compression *)
-  Context
-    (fun data ->
-      let { loc; level; vars } = data in
-      (* TODO: we could track substitution locations here for LSP *)
-      match Var.Map.find_opt var vars with
-      | Some (V_hole { level = _ }) ->
-          let info = V_link { type_ = to_ } in
-          let vars = Var.Map.add var info vars in
-          Ok ((), { loc; level; vars })
-      | Some ((V_rigid _ | V_alias _ | V_link _) as info) ->
-          let desc = CError_invalid_var_link { var; info } in
-          Error (CError { loc; desc }, data)
-      | None ->
-          let desc = CError_unknown_var_link { var } in
-          Error (CError { loc; desc }, data))
-
-let var_lower var ~to_ =
-  Context
-    (fun data ->
-      let { loc; level; vars } = data in
-      (* TODO: we could track lowering locations here for LSP *)
-      match Var.Map.find_opt var vars with
-      | Some (V_hole { level = hole_level }) -> (
-          match Level.(hole_level < to_) with
-          | true ->
-              let desc = CError_lowering_to_higher_level { var } in
-              Error (CError { loc; desc }, data)
-          | false ->
-              let info = V_hole { level = to_ } in
-              let vars = Var.Map.add var info vars in
-              Ok ((), { loc; level; vars }))
-      | Some ((V_rigid _ | V_alias _ | V_link _) as info) ->
-          let desc = CError_invalid_var_lower { var; info } in
-          Error (CError { loc; desc }, data)
-      | None ->
-          let desc = CError_unknown_var_lower { var } in
-          Error (CError { loc; desc }, data))
-
-let var_alias var ~of_ =
-  Context
-    (fun data ->
-      let { loc; level; vars } = data in
-      (* TODO: we could track lowering locations here for LSP *)
-      match Var.Map.find_opt var vars with
-      | Some (V_hole { level = _ }) ->
-          (* TODO: check level is equal or higher than of_ *)
-          let info = V_alias { var = of_ } in
-          let vars = Var.Map.add var info vars in
-          Ok ((), { loc; level; vars })
-      | Some ((V_rigid _ | V_alias _ | V_link _) as info) ->
-          let desc = CError_invalid_var_alias { var; info } in
-          Error (CError { loc; desc }, data)
-      | None ->
-          let desc = CError_unknown_var_alias { var } in
-          Error (CError { loc; desc }, data))
+  let[@inline always] with_binder f =
+    let context ~from ~to_ ~ok ~error =
+      let from = Offset.(from + one) in
+      let { context } = f () in
+      context ~from ~to_ ~ok ~error
+    in
+    { context }
+end
--- teika/context.mli
+++ teika/context.mli
@@ -1,68 +1,32 @@
 open Ttree
 
-(* TODO: maybe call it var_desc? *)
-
-(** This describes which kind of variable are we dealing with
-      - V_rigid are universal variables, they cannot move
-      - V_alias are used when binding two rigid variables
-          it is also used when unifying two hole variables
-      - V_link are hole variables that were unified 
-      - V_hole are existential variables, they can move 
-          they can also be linked *)
-type var_info = private
-  | V_rigid of { level : Level.t }
-  | V_alias of { var : Var.t }
-  (* TODO: this shouldy be type_ : type_ *)
-  | V_link of { type_ : term_desc }
-  | V_hole of { level : Level.t }
-
 type error = private CError of { loc : Location.t; desc : error_desc }
+and error_desc = private |
 
-and error_desc = private
-  (* unification *)
-  | CError_occurs_check of { var : Var.t; in_ : Var.t }
-  | CError_escape_check of { var : Var.t; to_ : Var.t }
-  | CError_var_constrained of { var : Var.t; by_ : term_desc }
-  | CError_var_clash of { expected : Var.t; received : Var.t }
-  | CError_type_clash of { expected : term_desc; received : term_desc }
-  (* invariants *)
-  | CError_unknown_var_repr of { var : Var.t }
-  | CError_unknown_var_link of { var : Var.t }
-  | CError_unknown_var_lower of { var : Var.t }
-  | CError_unknown_var_alias of { var : Var.t }
-  | CError_duplicated_var_rigid of { var : Var.t }
-  | CError_duplicated_var_alias of { var : Var.t }
-  | CError_duplicated_var_hole of { var : Var.t }
-  | CError_invalid_var_link of { var : Var.t; info : var_info }
-  | CError_invalid_var_lower of { var : Var.t; info : var_info }
-  | CError_invalid_var_alias of { var : Var.t; info : var_info }
-  | CError_lowering_to_higher_level of { var : Var.t }
+module Subst_context : sig
+  type 'a subst_context
+  type 'a t = 'a subst_context
 
-type 'a context
-type 'a t = 'a context
+  (* monad *)
+  val test :
+    loc:Warnings.loc ->
+    from:Offset.offset ->
+    to_:term_desc ->
+    (unit -> 'a t) ->
+    ('a, error) result
 
-val return : 'a -> 'a context
-val ( >>= ) : 'a context -> ('a -> 'b context) -> 'b context
-val fail_occurs_check : Var.t -> in_:Var.t -> 'a context
-val fail_escape_check : Var.t -> to_:Var.t -> 'a context
-val fail_var_constrained : Var.t -> by_:term_desc -> 'a context
-val fail_var_clash : expected:Var.t -> received:Var.t -> 'a context
-val fail_type_clash : expected:term_desc -> received:term_desc -> 'a context
+  val return : 'a -> 'a subst_context
+  val bind : 'a subst_context -> ('a -> 'b subst_context) -> 'b subst_context
 
-(* read *)
-val loc : Location.t context
-val level : Level.t context
-val repr : Var.t -> var_info context
+  val ( let* ) :
+    'a subst_context -> ('a -> 'b subst_context) -> 'b subst_context
 
-(* update *)
-val with_loc : Location.t -> (unit -> 'a context) -> 'a context
-val with_region : (unit -> 'a context) -> 'a context
-val with_var_rigid : Var.t -> (unit -> 'a context) -> 'a context
-val with_var_alias : Var.t -> of_:Var.t -> (unit -> 'a context) -> 'a context
+  val ( let+ ) : 'a subst_context -> ('a -> 'b) -> 'b subst_context
 
-(* unification *)
-val enter_var_hole : Var.t -> unit context
-val var_link : Var.t -> to_:term_desc -> unit context
-val var_lower : Var.t -> to_:Level.t -> unit context
-val var_alias : Var.t -> of_:Var.t -> unit context
-(* TODO: generalize *)
+  (* from *)
+  val from : unit -> Offset.t subst_context
+  val with_binder : (unit -> 'a subst_context) -> 'a subst_context
+
+  (* to_ *)
+  val to_ : unit -> term_desc subst_context
+end
