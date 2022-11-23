6150243d44af32a4d44caa45d7edee7a3e825645
EduardoRFS <theeduardorfs@gmail.com>
2022-11-23 18:04:35 -0300

    teika: reenable term substitution

--- teika/subst.ml
+++ teika/subst.ml
@@ -1,99 +1,70 @@
 open Ttree
+open Context
+open Subst_context
 
-let rec subst_term ~from ~to_ term =
+(* TODO: also do substitution using mutation *)
+let rec subst_term term =
   let (TTerm { loc; desc; type_ }) = term in
-  let desc = subst_desc ~from ~to_ desc in
+  let+ desc = subst_desc desc in
   TTerm { loc; desc; type_ }
 
-and subst_type ~from ~to_ type_ =
+and subst_type type_ =
   let (TType { loc; desc }) = type_ in
-  let desc = subst_desc ~from ~to_ desc in
+  let+ desc = subst_desc desc in
   TType { loc; desc }
 
-and subst_annot ~from ~to_ annot =
+and subst_annot : type a. _ -> (_ -> a subst_context) -> a subst_context =
+ fun annot k ->
   let (TAnnot { loc; var; annot }) = annot in
-  let annot = subst_type ~from ~to_ annot in
-  (var, tannot loc ~var ~annot)
+  let* annot = subst_type annot in
+  with_binder @@ fun () -> k (tannot loc ~var ~annot)
 
-and subst_bind ~from ~to_ bind =
+and subst_bind : type a. _ -> (_ -> a subst_context) -> a subst_context =
+ fun bind k ->
   let (TBind { loc; var; value }) = bind in
-  let value = subst_term ~from ~to_ value in
-  (var, tbind loc ~var ~value)
+  let* value = subst_term value in
+  with_binder @@ fun () -> k (tbind loc ~var ~value)
 
-and subst_desc ~from ~to_ desc =
-  let subst_term term = subst_term ~from ~to_ term in
-  let subst_type type_ = subst_type ~from ~to_ type_ in
-  let subst_annot annot = subst_annot ~from ~to_ annot in
-  let subst_bind bind = subst_bind ~from ~to_ bind in
+and subst_desc desc =
   match desc with
-  | TT_var { var } -> (
-      match Var.equal from var with true -> to_ | false -> TT_var { var })
+  | TT_var { offset } -> (
+      let* from = from () in
+      let+ to_ = to_ () in
+      match Offset.equal from offset with
+      | true -> to_
+      | false -> TT_var { offset })
   | TT_forall { param; return } ->
-      let var, param = subst_annot param in
-      let return =
-        match Var.equal from var with
-        | true -> return
-        | false -> subst_type return
-      in
+      subst_annot param @@ fun param ->
+      let+ return = subst_type return in
       TT_forall { param; return }
   | TT_lambda { param; return } ->
-      let var, param = subst_annot param in
-      let return =
-        match Var.equal from var with
-        | true -> return
-        | false -> subst_term return
-      in
+      subst_annot param @@ fun param ->
+      let+ return = subst_term return in
       TT_lambda { param; return }
   | TT_apply { lambda; arg } ->
-      let lambda = subst_term lambda in
-      let arg = subst_term arg in
+      let* lambda = subst_term lambda in
+      let+ arg = subst_term arg in
       TT_apply { lambda; arg }
   | TT_exists { left; right } ->
-      let var, left = subst_annot left in
-      let right =
-        match Var.equal from var with
-        | true -> right
-        | false ->
-            let _var, right = subst_annot right in
-            right
-      in
-      TT_exists { left; right }
+      subst_annot left @@ fun left ->
+      subst_annot right @@ fun right -> return @@ TT_exists { left; right }
   | TT_pair { left; right } ->
-      let var, left = subst_bind left in
-      let right =
-        match Var.equal from var with
-        | true -> right
-        | false ->
-            let _var, right = subst_bind right in
-            right
-      in
-      TT_pair { left; right }
+      subst_bind left @@ fun left ->
+      subst_bind right @@ fun right -> return @@ TT_pair { left; right }
   | TT_unpair { left; right; pair; return } ->
-      let pair = subst_term pair in
-      let return =
-        (* TODO: is this guard needed? *)
-        match Var.equal from left || Var.equal from right with
-        | true -> return
-        | false -> subst_term return
-      in
+      let* pair = subst_term pair in
+      with_binder @@ fun () ->
+      with_binder @@ fun () ->
+      let+ return = subst_term return in
       TT_unpair { left; right; pair; return }
   | TT_let { bound; return } ->
-      let var, bound = subst_bind bound in
-      let return =
-        match Var.equal from var with
-        | true -> return
-        | false -> subst_term return
-      in
+      subst_bind bound @@ fun bound ->
+      let+ return = subst_term return in
       TT_let { bound; return }
   | TT_annot { value; annot } ->
-      let annot = subst_type annot in
-      let value = subst_term value in
+      let* annot = subst_type annot in
+      let+ value = subst_term value in
       TT_annot { value; annot }
 
-let subst_annot ~from ~to_ annot =
-  let _var, annot = subst_annot ~from ~to_ annot in
-  annot
-
-let subst_bind ~from ~to_ bind =
-  let _var, bind = subst_bind ~from ~to_ bind in
-  bind
+let subst_annot annot = subst_annot annot @@ fun annot -> return @@ annot
+let subst_bind bind = subst_bind bind @@ fun bind -> return @@ bind
--- teika/subst.mli
+++ teika/subst.mli
@@ -1,7 +1,8 @@
 open Ttree
+open Context
 
-val subst_term : from:Var.t -> to_:term_desc -> term -> term
-val subst_type : from:Var.t -> to_:term_desc -> type_ -> type_
-val subst_desc : from:Var.t -> to_:term_desc -> term_desc -> term_desc
-val subst_annot : from:Var.t -> to_:term_desc -> annot -> annot
-val subst_bind : from:Var.t -> to_:term_desc -> bind -> bind
+val subst_term : term -> term Subst_context.t
+val subst_type : type_ -> type_ Subst_context.t
+val subst_desc : term_desc -> term_desc Subst_context.t
+val subst_annot : annot -> annot Subst_context.t
+val subst_bind : bind -> bind Subst_context.t
