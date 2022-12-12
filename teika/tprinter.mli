9849b4db21aa3703e8634a306e56c6860c6bf4cf
EduardoRFS <theeduardorfs@gmail.com>
2022-12-12 11:38:58 -0300

    teika: add simple printer for ttree

---
+++ teika/tprinter.mli
@@ -0,0 +1,4 @@
+open Ttree
+
+val pp_term : Format.formatter -> term -> unit
+val pp_pat : Format.formatter -> pat -> unit
--- teika/ttree.mli
+++ teika/ttree.mli
@@ -1,6 +1,8 @@
 (* TODO: make this private again? *)
 
 type term =
+  | TT_loc of { term : term; loc : Location.t }
+  | TT_offset of { term : term; offset : Offset.t }
   (* x *)
   | TT_var of { offset : Offset.t }
   (* (x : A) -> B *)
@@ -11,13 +13,10 @@ type term =
   | TT_apply of { lambda : term; arg : term }
   (* (v : T) *)
   | TT_annot of { term : term; annot : term }
-  | TT_offset of { term : term; offset : Offset.t }
-  | TT_loc of { term : term; loc : Location.t }
 
 and pat =
+  | TP_loc of { pat : pat; loc : Location.t }
   (* x *)
   | TP_var of { var : Name.t }
   (* (p : T) *)
   | TP_annot of { pat : pat; annot : term }
-  | TP_loc of { pat : pat; loc : Location.t }
-[@@deriving show]
