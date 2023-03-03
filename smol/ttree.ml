0c0b73a3289f8fdb19a45f7afaf9f331f8ea356e
Eduardo Rafael <theeduardorfs@gmail.com>
2023-03-03 20:46:22 +0000

    smol: add machinery to handle self types (#124)

--- smol/ttree.ml
+++ smol/ttree.ml
@@ -12,7 +12,7 @@ type _ term =
   | TT_lambda : { param : typed pat; return : _ term } -> core term
   | TT_apply : { lambda : _ term; arg : _ term } -> core term
   | TT_self : { bound : _ pat; body : _ term } -> core term
-  | TT_fix : { bound : _ pat; body : _ term } -> core term
+  | TT_fix : { bound : typed pat; body : _ term } -> core term
   | TT_unroll : { term : _ term } -> core term
 
 and _ pat =
