4a4004681fed00d9e5dccea16c33207a06349f4d
EduardoRFS <theeduardorfs@gmail.com>
2023-07-22 20:46:52 -0300

    teika: simple explicit open / close / subst

--- teika/ttree.mli
+++ teika/ttree.mli
@@ -1,4 +1,5 @@
 type loc = Loc
+type subst = Subst
 type typed = Typed
 type core = Core
 type sugar = Sugar
@@ -6,6 +7,20 @@ type sugar = Sugar
 type _ term =
   | TT_loc : { term : _ term; loc : Location.t } -> loc term
   | TT_typed : { term : _ term; annot : _ term } -> typed term
+  (* M[-n := N]*)
+  | TT_subst_bound : {
+      from : Index.t;
+      to_ : _ term;
+      term : _ term;
+    }
+      -> subst term
+  (* M[+f `close` -t]*)
+  | TT_close_free : {
+      from : Level.t;
+      to_ : Index.t;
+      term : _ term;
+    }
+      -> subst term
   (* x/-n *)
   | TT_bound_var : { index : Index.t } -> core term
   (* x/+n *)
@@ -31,6 +46,8 @@ val nil_level : Level.t
 val type_level : Level.t
 
 (* constructors *)
+val tt_subst_bound : from:Index.t -> to_:_ term -> _ term -> subst term
+val tt_close_free : from:Level.t -> to_:Index.t -> _ term -> subst term
 val tt_nil : core term
 val tt_type : core term
 val tt_hole : unit -> core term
