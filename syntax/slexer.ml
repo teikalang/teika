062b995c251ef8d21fe87073613dd7208d23af22
EduardoRFS <theeduardorfs@gmail.com>
2023-11-13 21:58:41 -0300

    syntax: copy slexer from teika

---
+++ syntax/slexer.ml
@@ -0,0 +1,47 @@
+open Sparser
+open Sedlexing.Utf8
+
+(* TODO: more information *)
+exception Lexer_unknown_token
+
+let whitespace = [%sedlex.regexp? Plus (' ' | '\t' | '\n')]
+let alphabet = [%sedlex.regexp? 'a' .. 'z' | 'A' .. 'Z']
+let digit = [%sedlex.regexp? '0' .. '9']
+let variable = [%sedlex.regexp? (alphabet | '_'), Star (alphabet | digit | '_')]
+let extension = [%sedlex.regexp? '%', variable]
+let string = [%sedlex.regexp? '"', Star (Sub (any, '"')), '"']
+
+let rec tokenizer buf =
+  match%sedlex buf with
+  | whitespace -> tokenizer buf
+  | variable -> VAR (lexeme buf)
+  | extension -> EXTENSION (lexeme buf)
+  | ":" -> COLON
+  | "->" -> ARROW
+  | "=>" -> FAT_ARROW
+  | "=" -> EQUAL
+  | "," -> COMMA
+  | "&" -> AMPERSAND
+  | ";" -> SEMICOLON
+  | string ->
+      (* TODO: this should probably be somewhere else *)
+      let literal = lexeme buf in
+      (* TODO: this is dangerous *)
+      let literal = String.sub literal 1 (String.length literal - 2) in
+      STRING literal
+  | "(" -> LEFT_PARENS
+  | ")" -> RIGHT_PARENS
+  | "{" -> LEFT_BRACE
+  | "}" -> RIGHT_BRACE
+  | eof -> EOF
+  | _ -> raise Lexer_unknown_token
+
+let provider buf () =
+  let token = tokenizer buf in
+  let start, stop = Sedlexing.lexing_positions buf in
+  (token, start, stop)
+
+let from_string parser string =
+  let buf = from_string string in
+  let provider = provider buf in
+  MenhirLib.Convert.Simplified.traditional2revised parser provider
