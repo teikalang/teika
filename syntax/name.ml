926228f7873cddd9d9d6517fe0aa227253e3d25c
EduardoRFS <theeduardorfs@gmail.com>
2023-11-13 21:55:45 -0300

    syntax: define library and copy parser

---
+++ syntax/name.ml
@@ -0,0 +1,8 @@
+type name = string
+and t = name [@@deriving show, eq, ord]
+
+let make t = t
+let repr t = t
+
+module Map = Map.Make (String)
+module Tbl = Hashtbl.Make (String)
---
+++ syntax/stree.mli
@@ -0,0 +1,30 @@
+type term =
+  | ST_loc of { loc : Location.t; term : term }
+  | ST_var of { var : Name.t }
+  | ST_extension of { extension : Name.t }
+  | ST_forall of { param : term; return : term }
+  | ST_lambda of { param : term; return : term }
+  | ST_apply of { lambda : term; arg : term }
+  | ST_pair of { left : term; right : term }
+  | ST_both of { left : term; right : term }
+  | ST_bind of { bound : term; value : term }
+  | ST_semi of { left : term; right : term }
+  | ST_annot of { value : term; annot : term }
+  | ST_string of { literal : string }
+  | ST_parens of { content : term }
+  | ST_braces of { content : term }
+[@@deriving show]
+
+val st_var : Location.t -> var:Name.t -> term
+val st_extension : Location.t -> extension:Name.t -> term
+val st_forall : Location.t -> param:term -> return:term -> term
+val st_lambda : Location.t -> param:term -> return:term -> term
+val st_apply : Location.t -> lambda:term -> arg:term -> term
+val st_pair : Location.t -> left:term -> right:term -> term
+val st_both : Location.t -> left:term -> right:term -> term
+val st_bind : Location.t -> bound:term -> value:term -> term
+val st_semi : Location.t -> left:term -> right:term -> term
+val st_annot : Location.t -> value:term -> annot:term -> term
+val st_string : Location.t -> literal:string -> term
+val st_parens : Location.t -> content:term -> term
+val st_braces : Location.t -> content:term -> term
