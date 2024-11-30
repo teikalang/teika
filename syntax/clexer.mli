418d8a3fc39f784f5e6316e50668d50c7f40e5bd
EduardoRFS <theeduardorfs@gmail.com>
2024-11-30 17:54:57 -0300

    syntax: incremental menhir and errors with locations

--- syntax/clexer.mli
+++ syntax/clexer.mli
@@ -1,2 +1,6 @@
-val from_string :
-  (Cparser.token, 'a) MenhirLib.Convert.traditional -> string -> 'a
+exception Lexer_error of { loc : Location.t }
+exception Parser_error of { loc : Location.t }
+
+val loc : Sedlexing.lexbuf -> Location.t
+val next : Sedlexing.lexbuf -> Cparser.token * Lexing.position * Lexing.position
+val term_opt_from_string : string -> Ctree.term option
