7fb01a5e6fb723a58589af59eeb4227ce6e9941b
EduardoRFS <theeduardorfs@gmail.com>
2024-12-02 19:10:12 -0300

    teika: tuples on Ttree and solving

--- teika/ttree.mli
+++ teika/ttree.mli
@@ -1,6 +1,7 @@
 open Utils
 
-type term = private Term of { struct_ : term_struct; loc : Location.t }
+(* TODO: explicit unfold for loops on terms *)
+type term = Term of { struct_ : term_struct; loc : Location.t [@opaque] }
 
 and term_struct =
   (* (M : A) *)
@@ -9,20 +10,30 @@ and term_struct =
   | T_var of { var : Index.t }
   (* P = N; M *)
   | T_let of { bound : pat; arg : term; body : term }
-  (* P : A; M *)
-  | T_hoist of { bound : pat; body : term }
-  (* P : A; ...; P = N; M *)
-  (* TODO: pattern on fix? *)
-  | T_fix of { bound : pat; var : Index.t; arg : term; body : term }
+  (* x : A; M *)
+  | T_hoist of { bound : var_pat; body : term }
+  (* x : A; ...; x = N; M *)
+  | T_fix of { bound : var_pat; var : Index.t; arg : term; body : term }
   (* P => M *)
   | T_lambda of { bound : pat; body : term }
   (* M N *)
   | T_apply of { funct : term; arg : term }
   (* (P : A) -> B *)
   | T_forall of { bound : pat; param : term; body : term }
-  (* TODO: part of fix *)
   (* (P : A) & B *)
-  | T_self of { bound : pat; body : term }
+  | T_self of { bound : var_pat; body : term }
+  (* (M, ...) *)
+  | T_tuple of { elements : term list }
+  (* (x : A, ...) *)
+  | T_exists of { elements : pat list }
+
+and var_pat = VPat of { struct_ : var_pat_struct; loc : Location.t }
+
+and var_pat_struct =
+  (* (P : A) *)
+  | VP_annot of { pat : var_pat; annot : term }
+  (* x *)
+  | VP_var of { var : Name.t }
 
 and pat = Pat of { struct_ : pat_struct; loc : Location.t }
 
@@ -30,8 +41,12 @@ and pat_struct =
   (* (P : A) *)
   | P_annot of { pat : pat; annot : term }
   (* x *)
+  (* TODO: drop names and uses receipts *)
   | P_var of { var : Name.t }
+  (* (x, ...) *)
+  | P_tuple of { elements : pat list }
 [@@deriving show]
 
 val t_wrap : loc:Location.t -> term_struct -> term
+val vp_wrap : loc:Location.t -> var_pat_struct -> var_pat
 val p_wrap : loc:Location.t -> pat_struct -> pat
